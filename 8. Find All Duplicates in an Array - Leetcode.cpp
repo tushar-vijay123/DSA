23ab6bc865b16c426c78b839a0c7485262bb7841
Tushar Vijay <97074208+tushar-vijay@users.noreply.github.com>
2024-06-10 19:54:27 +0530

    Create 8. Find All Duplicates in an Array - Leetcode.cpp

---
+++ 8. Find All Duplicates in an Array - Leetcode.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        int len = nums.size();
+
+        for(int i=0; i<len; i++)
+        {
+            int el = nums[abs(nums[i]) - 1];
+            if(el < 0)
+            {
+                ans.push_back(abs(nums[i]));
+            }
+            else
+            {
+                nums[abs(nums[i]) - 1] *= (-1);
+            }
+        }
+
+        return ans;
+    }
+};

9c7acef32fa2be19317f6125285c53e5abdc12c8
Tushar Vijay <97074208+tushar-vijay@users.noreply.github.com>
2024-10-08 20:20:58 +0530

    Update 20. Aggressive Cows - Naukri Code 360.cpp

--- 20. Aggressive Cows - Naukri Code 360.cpp
+++ 20. Aggressive Cows - Naukri Code 360.cpp
@@ -1,47 +1,44 @@
-bool isPossible(vector<int> &stalls, int n, int k, int mid)
-{
-    int cowCount = 1;
+bool isPossible(vector<int> stalls, int n, int k, int minAllowedDist) {
+    sort(stalls.begin(), stalls.end());
+
+    int cows = 1;
     int lastPos = stalls[0];
 
-    for(int i=0; i<n; i++)
-    {
-        if(stalls[i] - lastPos >= mid)
-        {
-            cowCount++;
-            if(cowCount == k)
-            {
-                return true;
-            }
+    for(int i = 1; i < n; i++) {
+        if(stalls[i] - lastPos >= minAllowedDist) {
+            cows++;
             lastPos = stalls[i];
         }
+        if(cows == k)
+            return true;
     }
+
     return false;
 }
 
 int aggressiveCows(vector<int> &stalls, int k)
 {
+    int maxi = stalls[0], mini = stalls[0];
     int n = stalls.size();
-    sort(stalls.begin(), stalls.end());
-    
-    int s = 0;
-    int e = stalls[n-1];
-    int mid = s + (e - s)/2;
+    for(int i = 0; i < n; i++) {
+        if(stalls[i] > maxi)
+            maxi = stalls[i];
+        if(stalls[i] < mini)
+            mini = stalls[i];
+    }
 
+    int s = 1, e = maxi - mini; // minimum & maximum distance -> range
     int ans = -1;
 
-    while(s <= e)
-    {
-        if(isPossible(stalls, n, k, mid))
-        {
+    while(s <= e) {
+        int mid = s + (e - s)/2;
+
+        if(isPossible(stalls, n, k, mid)) {
             ans = mid;
             s = mid + 1;
-        }
-        else
-        {
+        } else {
             e = mid - 1;
         }
-
-        mid = s + (e - s)/2;
     }
 
     return ans;

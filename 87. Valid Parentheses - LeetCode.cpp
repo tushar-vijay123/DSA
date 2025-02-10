4a00bad3287a17172f0a4e36b030201d04249f0d
Tushar Vijay <97074208+tushar-vijay@users.noreply.github.com>
2025-02-10 16:11:27 +0530

    Create 87. Valid Parentheses - LeetCode.cpp

---
+++ 87. Valid Parentheses - LeetCode.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }else{
+                if(st.empty()){
+                    return false;
+                }
+
+                if((st.top() == '(' && s[i] == ')') ||
+                    (st.top() == '{' && s[i] == '}') ||
+                    (st.top() == '[' && s[i] == ']')){
+                        st.pop();
+                }else{ // no match
+                    return false;
+                }
+            }
+        }
+
+        return st.size() == 0;
+    }
+};

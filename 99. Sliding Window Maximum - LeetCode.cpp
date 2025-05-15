11c5a94eba8bcdcafbad31dce4e883e630ef78a5
Tushar Vijay <97074208+tushar-vijay123@users.noreply.github.com>
2025-05-15 17:07:46 +0530

    Create 99. Sliding Window Maximum - LeetCode.cpp

---
+++ 99. Sliding Window Maximum - LeetCode.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> res;
+
+        // 1st window
+        for(int i = 0; i < k; i++){
+            while(dq.size() > 0 && nums[dq.back()] <= nums[i]){
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+        }
+
+        for(int i = k; i < nums.size(); i++){
+            res.push_back(nums[dq.front()]);
+
+            // remove elements that are not part of curr window
+            while(dq.size() > 0 && dq.front() <= i-k){
+                dq.pop_front();
+            }
+
+            // remove the smaller values
+            while(dq.size() > 0 && nums[dq.back()] <= nums[i]){
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+        }
+
+        res.push_back(nums[dq.front()]);
+
+        return res;
+    }
+};
